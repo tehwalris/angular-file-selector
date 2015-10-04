5da6cf8d9849d3e6d2672a70de483ea26a2f8538
Philippe Voinov <philippevoinov@gmail.com>
2015-10-04 15:45:16 +0200

    Initial commit.

---
+++ angular-file-selector.js
@@ -0,0 +1,27 @@
+angular.module('tehwalris.file-selector', [])
+.directive('fileSelector', function () {
+  return {
+      restrict: 'A',
+      transclude: true,
+      scope: {
+        onSelect: '&?',
+        file: '=?'
+      },
+      link: {post: linkFunc},
+      template: '<input style="display:none;" type="file" /><div ng-transclude></div>'
+  };
+
+  function linkFunc(scope, element) {
+    var fileInput = element.find('input')[0];
+    element.bind('click', function () { 
+      fileInput.focus();
+      fileInput.click();
+    });
+    element.bind('change', function (event) {
+      var file = event.target.files[0];
+      scope.onSelect({file: file});
+      scope.file = file;
+      scope.$apply(); //May not update file immediately otherwise
+    });
+  }
+});
